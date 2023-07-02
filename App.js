e3240bdc0c87dc38106313b082c920e8c414bb49
rizal2302 <84950055+rizal2302@users.noreply.github.com>
2023-07-03 00:32:10 +0800

    Update geolocation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   Button,
   PermissionsAndroid,
+  Platform,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -14,23 +15,27 @@ const App = () => {
 
   const requestLocationPermission = async () => {
     try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Geolocation Permission',
-          message: 'Can we access your location?',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK'
-        }
-      );
-      console.log('granted', granted);
-      if (granted === 'granted') {
-        console.log('You can use Geolocation');
+      if (Platform.OS === 'ios') {
         return true;
       } else {
-        console.log('You cannot use Geolocation');
-        return false;
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Geolocation Permission',
+            message: 'Can we access your location?',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK'
+          }
+        );
+        console.log('granted', granted);
+        if (granted === 'granted') {
+          console.log('You can use Geolocation');
+          return true;
+        } else {
+          console.log('You cannot use Geolocation');
+          return false;
+        }
       }
     } catch (err) {
       return false;
